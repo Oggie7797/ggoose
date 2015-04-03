bb18e6227a613e24c124c5e72f781bf1d65b7f92
Oggie7797 <oggie7797@gmail.com>
2015-04-03 12:48:36 -0500

    first commit

---
+++ src/main/java/com/empz/ggoose/Gold.java
@@ -0,0 +1,11 @@
+package com.empz.ggoose;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Gold implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
